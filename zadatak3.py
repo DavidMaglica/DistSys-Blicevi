be1c31f370d3e1122a66a49fc09c6075ed4747c6
DavidMaglica <davidmaglica@protonmail.ch>
2022-10-24 16:03:31 +0200

    Pravilno rješenje zadatka 3

--- zadatak3.py
+++ zadatak3.py
@@ -4,8 +4,10 @@ def fn(listOfDicts):
     i ako su svi dict ako ne error. Proizvod ima naziv, kategoriju i ocjenu
     Vraća dict s kategorijom za key i sumom ocjena (Jedan ili dva one linera)
     """
-    return({item["kategorija"]: "a", item["kategorija"]: "b"} for item in listOfDicts if isinstance(listOfDicts, list) and isinstance(item, dict))
-
+    # return({item["kategorija"]: "a", item["kategorija"]: "b"} for item in listOfDicts if isinstance(listOfDicts, list) and isinstance(item, dict))
+    assert isinstance(listOfDicts, list)
+    c = {k for d in listOfDicts for a, k in d.items() if a == "kategorija"}
+    return{k:sum([v["ocjena"] for v in listOfDicts if v["kategorija"] == k]) for d in listOfDicts for _, k in d.items() if k in c}
 
 print(fn([{"naziv": "Burek", "kategorija": "pite", "ocjena": 1},
         {"naziv": "Ramsteak", "kategorija": "steak", "ocjena": 9},
