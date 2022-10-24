91a197b477697c7e4f8eb6ba91a54017b1ffc001
DavidMaglica <davidmaglica@protonmail.ch>
2022-10-24 15:29:21 +0200

    Pravilno rješenje prvog zadatka

--- zadatak1.py
+++ zadatak1.py
@@ -4,6 +4,8 @@ def fn(ls):
     stringovi ako ne error. Kreira dictionary gdje je key index, a value obrnuti
     string (One-liner u return-u)
     """
-    return({_: item for item in ls if isinstance(ls, list) and isinstance(item, str)})
+    # return({_: item for item in ls if isinstance(ls, list) and isinstance(item, str)})
+    assert isinstance(x, list) and all([isinstance(i, str) for i in ls])
+    return {k:v[::-1] for k, v in enumerate(ls)}
 
 print(fn(["Stol", "Stolica", "Krevet", "Fotelja"]))
